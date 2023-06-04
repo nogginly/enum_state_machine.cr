c2f9d205167545981d734722487d50d7e9472927
nogginly <nogginly@icloud.com>
2023-06-03 20:52:59 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
-# enum_state_machine.cr
-A type-safe finite state machine for Crystal where the states are defined using `enum`'s.
+# Enum State Machine for Crystal
+
+`enum_state_machine` is a type-safe finite state machine for Crystal where the states are defined using enum's. I created it because I wanted the minimal FSM data type that was function and easy to use.
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     enum_state_machine:
+       github: nogginly/enum_state_machine.cr
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+Here's a single example that illustrates how to use `enum_state_machine` to setup a toy `Player` class with two statuses, `Action` and `Health`, and their respective state machines.
+
+```cr
+require "enum_state_machine"
+
+module MyGame
+  enum Action
+    Idle
+    Walk
+    Run
+    Jump
+    Attack
+  end
+
+  enum Health
+    Alive
+    Dead
+  end
+
+  class Player
+    include EnumStateMachine
+
+    state_machine(Action, initial: Action::Idle) do
+      event :idle,        # from any state except these
+        except_from: {Action::Idle, Action::Jump}, to: Action::Idle
+      event :jump,        # from one of many states
+        from: {Action::Walk, Action::Run, Action::Idle}, to: Action::Jump
+      event :attack,      # from one state
+        from: Action::Idle, to: Action::Attack
+      event :run, to: Action::Run
+    end
+
+    state_machine(Health, initial: Health::Alive) do
+      event :die,         # from one state
+        from: Health::Alive, to: Health::Dead
+      event :resurrect,   # from a state, with a guard
+        from: Health::Dead, to: Health::Alive,
+        guard: ->{ idle? }
+    end
+  end
+```
+
+Each event `do_this` adds the following capability methods:
+
+- `do_this` attempts to perform the transition
+- `do_this(&)` which yields after successfully performing transition
+- `may_do_this?` returns true of the transition may be performed
+
+Each state machine for an `enum Feeling` adds the following methods:
+
+- `feeling` which returns the current value of the `Feeling` state
+- for each enumeration of `Feeling`, say `Feeling::Kind` and `Feeling::Sad`
+  - `kind?` which returns true of the current value of `feeling` is `Feeling::Kind`
+  - `sad?`
+  - and so on.
+
+## To do
+
+_(In no particular order)_
+
+- More examples for the various ways to define and use `enum_state_machine`
+- Document possible compile errors
+- Event transition failure handler (like `guard`)
+- Multiple transitions for a single event
+- Test suite
+
+## Contributing
+
+Bug reports and sugestions are welcome. Otherwise, at this time, this project is closed for code changes and pull requests. I appreciate your understanding.
+
+This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [code of conduct](https://www.contributor-covenant.org/version/1/4/code-of-conduct/).
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
+
+## Contributors
+
+- [nogginly](https://github.com/nogginly) - creator and maintainer
---
+++ src/enum_state_machine.cr
@@ -0,0 +1,190 @@
+#
+# Define type-safe finite state machine for Crystal where the states are defined using enum's.
+#
+# ```
+# enum Health
+#   Alive
+#   Dead
+#   Zombie
+# end
+#
+# class Player
+#   include EnumStateMachine
+#
+#   getter hp = 100
+#
+#   state_machine(Health, initial: Health::Alive) do
+#     event :die, to: Health::Dead
+#     event :resurrect, from: Health::Dead, to: Health::Alive, guard: ->{ hp <= 0 }
+#     event :succumb, except_from: Health::Alive, to: Health::Zombie
+#   end
+# end
+# ```
+#
+module EnumStateMachine
+  # :nodoc:
+  # Internal state machine used to set up the scaffolding
+  # for the generated FSM
+  class EFSM(E)
+    @state : E
+
+    protected def initialize(@state); end
+
+    @[AlwaysInline]
+    protected def state : E
+      @state
+    end
+
+    @[AlwaysInline]
+    protected def is?(s : E) : Bool
+      state == s
+    end
+
+    @[AlwaysInline]
+    protected def is?(s : Tuple | Array) : Bool
+      s.any? { |a| state == a }
+    end
+
+    @[AlwaysInline]
+    protected def not?(s : E) : Bool
+      state != s
+    end
+
+    @[AlwaysInline]
+    protected def not?(s : Tuple | Array) : Bool
+      s.all? { |a| state != a }
+    end
+
+    @[AlwaysInline]
+    protected def transitions(*, to : E) : E
+      @state = to
+    end
+
+    @[AlwaysInline]
+    protected def transitions(*, from, to : E) : E?
+      @state = to if is?(from)
+    end
+
+    @[AlwaysInline]
+    protected def transitions(*, except_from, to : E) : E?
+      @state = to if not?(except_from)
+    end
+
+    @[AlwaysInline]
+    protected def allowed?(&)
+      return true if yield
+      false
+    end
+
+    # :nodoc:
+    macro event(state_enum, field, name, *, from = nil, except_from = nil, to, guard)
+      {%
+        if (from && except_from)
+          raise "State machine event requires either `from` or `except` or neither, but not both."
+        end
+      %}
+
+      macro guard_{{name.id}}
+        {% if guard.is_a?(ProcLiteral) %}
+          {%
+            raise "Event's guard handler (proc) cannot expect arguments." if guard.args.size > 0
+          %}
+          return nil unless (@{{field}}.allowed? do
+            {{guard.body}}
+          end)
+        {% end %}
+      end
+
+      # Check if `{{state_enum}}` state can transition to `#{{to}}`
+      def {{ "may_#{name.id}?".id }} : Bool
+        guard_{{name.id}}
+        {% if from %}
+        @{{field}}.is?({{from}})
+        {% elsif except_from %}
+        @{{field}}.not?({{except_from}})
+        {% else %}
+        true
+        {% end %}
+      end
+
+      # Transition `{{state_enum}}` state to `#{{to}}` if possible; return nil if unable.
+      def {{ name.id }} : {{state_enum}}?
+        guard_{{name.id}}
+        {% if from %}
+        @{{field}}.transitions(from: {{from}}, to: {{to}})
+        {% elsif except_from %}
+        @{{field}}.transitions(except_from: {{except_from}}, to: {{to}})
+        {% else %}
+        @{{field}}.transitions(to: {{to}})
+        {% end %}
+      end
+
+      # Transition `{{state_enum}}` state to `#{{to}}` and yield to block if possible; return nil if unable.
+      def {{ name.id }}(&) : {{state_enum}}?
+        if (outcome = {{ name.id }})
+          yield
+        end
+        outcome
+      end
+    end
+  end
+
+  # Setup a state machine based on *state_enum* enumeration, with *initial* value, and *block* containing the
+  # `event` declarations.
+  #
+  # > **Example:**, given `enum Health` with values `Alive` and `Dead`, a `state_machine`
+  # declared with an event `:die` that transitions to `Dead` makes the following methods available:
+  # >
+  # > - `heath` to get the current state
+  # > - `dead?` to check if state is `Dead`
+  # > - `alive?` to check if state is `Alive`
+  # > - `die` to make the transition if possible
+  # > - `may_die?` to check if player can `die`
+  #
+  # Using different `enum` types, multiple state machines can be setup in the same object.
+  macro state_machine(state_enum, *, initial, &block)
+    # Make sure state is an Enum
+    {%
+      state_type = state_enum.resolve
+      unless state_type.ancestors.includes?(Enum)
+        raise "State machine requires an Enum; `{{state_enum}}` isn't one."
+      end
+    %}
+
+    {% machine = "EFSM(#{state_enum})".id %}
+    {% field = "#{state_enum.id.underscore}_sm".id %}
+
+    # The internal `{{state_enum}}` state machine
+    @{{ field }} = {{machine}}.new({{ initial }})
+
+    # The current value of the `{{state_enum}}` state.
+    def {{state_enum.id.underscore}} : {{state_enum}}
+      {{field}}.state
+    end
+
+    # Internal delegating macro
+    macro event(name, *, to, guard=nil)
+      EFSM.event({{state_enum}}, {{field}}, \{{name}}, to: \{{to}}, guard: \{{guard}})
+    end
+
+    # Internal delegating macro
+    macro event(name, *, from, to, guard=nil)
+      EFSM.event({{state_enum}}, {{field}}, \{{name}}, from: \{{from}}, to: \{{to}}, guard: \{{guard}})
+    end
+
+    # Internal delegating macro
+    macro event(name, *, except_from, to, guard=nil)
+      EFSM.event({{state_enum}}, {{field}}, \{{name}}, except_from: \{{except_from}}, to: \{{to}}, guard: \{{guard}})
+    end
+
+    {{ block.body }}
+
+    {% for e in state_type.constants %}
+      {% e_name = e.id.underscore %}
+      # Check if current `{{state_enum}}` state is `{{e.id}}`
+      def {{e_name}}? : Bool
+        @{{ field }}.state.{{e_name}}?
+      end
+    {% end %}
+  end
+end
